a626f13ffc6b3cea9afb3a266a732edbf44d94cb
eyansibi <eya.nsibi@esprit.tn>
2025-11-14 16:14:04 +0100

    ajuster login logout

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('dashboard/', views.backoffice_dashboard, name='backoffice_dashboard'),
     # === BackOffice (admin) ===
     path('backoffice/', views.backoffice_dashboard, name='backoffice_dashboard'),
     path('backoffice/users/', views.backoffice_users, name='backoffice_users'),
